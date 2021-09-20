2f4e0c38ec45e567071fd0299242c05b20e2715f
paulow540 <halleluyahpaul@gmail.com>
2021-09-20 23:44:58 +0100

    i added a upload function and a page to the adminapp

--- Ecommance/adminapp/views.py
+++ Ecommance/adminapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from.forms import SignUpForm
+from django.urls import reverse_lazy, reverse
+from.forms import SignUpForm ,Product_form
 from django.views import generic
 from .models import Product_table
+from django.http import HttpResponseRedirect , HttpResponsePermanentRedirect 
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 class SignUpView(generic.CreateView):
@@ -11,5 +14,17 @@ class SignUpView(generic.CreateView):
     template_name = 'registration/signup.html'
 
 
+@login_required
 def upload_product(request):
-    pass
+    if request.method == 'POST':
+        form = Product_form(request.POST, request.FILES)
+        if form.is.valid():
+            post = form.save(commit=False)
+
+
+
+@login_required
+def manage_product(request):
+    product_details = Product_table.objects.all()
+    return render(request, 'adminapp/manage_product.html')
+    
